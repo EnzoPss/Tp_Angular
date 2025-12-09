d64e3882f1f60b94167beb07950037446c62e273
EnzoPss <enzopousse.06@gmail.com>
2025-12-09 11:57:42 +0100

    debut projet3 - hackers app->footer

---
+++ hackers-app/src/app/app-module.ts
@@ -0,0 +1,26 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './component/header/header';
+import { Footer } from './component/footer/footer';
+import { Home } from './component/home/home';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Footer,
+    Home
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
